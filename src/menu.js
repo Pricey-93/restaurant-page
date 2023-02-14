b48399e16112df096ca03c48e948aefcea7cd6a1
Dan <danieljamieprice@gmail.com>
2023-02-14 20:08:44 +0900

    Add function for creating menu

---
+++ src/menu.js
@@ -0,0 +1,21 @@
+function createMenuItem(title, description) {
+  const container = document.createElement('div');
+  const image = document.createElement('img');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+  container.classList.add('menu-item');
+  h2.textContent = title;
+  p.textContent = description;
+  container.appendChild(image);
+  container.appendChild(h2);
+  container.appendChild(p);
+  return container;
+}
+
+export default function createMenu() {
+  const container = document.createElement('div');
+  container.id = 'menu-container';
+  container.appendChild(createMenuItem('pepperoni pizza', 'This is a real good pizza'));
+  container.appendChild(createMenuItem('pinapple pizza', 'Only uncultured people disagree with this pizza'));
+  return container;
+}
