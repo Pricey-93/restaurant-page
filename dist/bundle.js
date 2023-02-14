cf68e5b2e43f57e7ed6632af697fe8f156d7f5be
Dan <danieljamieprice@gmail.com>
2023-02-14 18:27:38 +0900

    Add function for creating navigation bar

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,32 @@
-(()=>{"use strict";console.log("this is a test")})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("function createNavigationBar() {\n  const header = document.createElement('header');\n  const h1 = document.createElement('h1');\n  const nav = document.createElement('nav');\n  const ul = document.createElement('ul');\n  const homeTab = document.createElement('li');\n  const menuTab = document.createElement('li');\n  const contactTab = document.createElement('li');\n  h1.textContent = 'Pizza Palace';\n  ul.appendChild(homeTab);\n  ul.appendChild(menuTab);\n  ul.appendChild(contactTab);\n  nav.appendChild(ul);\n  header.appendChild(h1);\n  header.appendChild(nav);\n  return header;\n}\n\nconst content = document.getElementById('content');\nconst navigationBar = createNavigationBar();\ncontent.appendChild(navigationBar);\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import test from './home';
+function createNavigationBar() {
+  const header = document.createElement('header');
+  const h1 = document.createElement('h1');
+  const nav = document.createElement('nav');
+  const ul = document.createElement('ul');
+  const homeTab = document.createElement('li');
+  const menuTab = document.createElement('li');
+  const contactTab = document.createElement('li');
+  h1.textContent = 'Pizza Palace';
+  ul.appendChild(homeTab);
+  ul.appendChild(menuTab);
+  ul.appendChild(contactTab);
+  nav.appendChild(ul);
+  header.appendChild(h1);
+  header.appendChild(nav);
+  return header;
+}
 
-test();
+const content = document.getElementById('content');
+const navigationBar = createNavigationBar();
+content.appendChild(navigationBar);
