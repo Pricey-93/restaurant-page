dcdbf8c2778ae3c68f30e105f30280a219654e0c
Dan <danieljamieprice@gmail.com>
2023-02-14 20:07:29 +0900

    Add function for creating home content

--- src/home.js
+++ src/home.js
@@ -1,4 +1,12 @@
-/* eslint-disable no-console */
-export default function test() {
-  console.log('this is a test');
+export default function createHomeContent() {
+  const container = document.createElement('div');
+  const topParagraph = document.createElement('p');
+  const image = document.createElement('img');
+  const bottomParagraph = document.createElement('p');
+  topParagraph.textContent = 'we\'ve been making the sickest pizzas since 1905!';
+  bottomParagraph.textContent = 'Order online or visit us in store!';
+  container.appendChild(topParagraph);
+  container.appendChild(image);
+  container.appendChild(bottomParagraph);
+  return container;
 }
