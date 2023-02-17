ccd6fc8805f4b1e8c304ad3cd6b1c2f2688b080c
Dan <danieljamieprice@gmail.com>
2023-02-17 22:56:39 +0900

    import lego pizza box image

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,14 @@
-function createMenuItem(title, description) {
+import legoPizzaBox from './lego-pizza-box.png';
+
+function createMenuItem(title, description, imageUrl) {
   const container = document.createElement('div');
   const image = document.createElement('img');
   const h2 = document.createElement('h2');
   const p = document.createElement('p');
   container.classList.add('menu-item');
+  image.src = imageUrl;
+  image.width = 100;
+  image.height = 100;
   h2.textContent = title;
   p.textContent = description;
   container.appendChild(image);
@@ -16,7 +21,14 @@ export default function createMenu() {
   const container = document.createElement('div');
   container.id = 'menu-content-container';
   container.classList.add('active-content');
-  container.appendChild(createMenuItem('pepperoni pizza', 'This is a real good pizza'));
-  container.appendChild(createMenuItem('pinapple pizza', 'Only uncultured people disagree with this pizza'));
+  container.appendChild(createMenuItem('pepperoni pizza', 'This is a real good and tasty pepperoni pizza', legoPizzaBox));
+  container.appendChild(createMenuItem('pineapple pizza', 'Only uncultured people disagree with this pizza', legoPizzaBox));
+  container.appendChild(createMenuItem('lorem ipsum pizza', 'lorem ipsum pizza containing the finest lorum ipsums', legoPizzaBox));
+  container.appendChild(createMenuItem('lorem ipsum pizza', 'lorem ipsum pizza containing the finest lorum ipsums', legoPizzaBox));
+  container.appendChild(createMenuItem('lorem ipsum pizza', 'lorem ipsum pizza containing the finest lorum ipsums', legoPizzaBox));
+  container.appendChild(createMenuItem('lorem ipsum pizza', 'lorem ipsum pizza containing the finest lorum ipsums', legoPizzaBox));
+  container.appendChild(createMenuItem('lorem ipsum pizza', 'lorem ipsum pizza containing the finest lorum ipsums', legoPizzaBox));
+  container.appendChild(createMenuItem('lorem ipsum pizza', 'lorem ipsum pizza containing the finest lorum ipsums', legoPizzaBox));
+  container.appendChild(createMenuItem('lorem ipsum pizza', 'lorem ipsum pizza containing the finest lorum ipsums', legoPizzaBox));
   return container;
 }
