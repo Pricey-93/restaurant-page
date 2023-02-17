96802e8c1de2d95039ba32b1acf284d2f5557fc6
Dan <danieljamieprice@gmail.com>
2023-02-17 19:42:38 +0900

    give id and class to home container

--- src/home.js
+++ src/home.js
@@ -3,6 +3,8 @@ export default function createHomeContent() {
   const topParagraph = document.createElement('p');
   const image = document.createElement('img');
   const bottomParagraph = document.createElement('p');
+  container.id = 'home-content-container';
+  container.classList.add('active-content');
   topParagraph.textContent = 'we\'ve been making the sickest pizzas since 1905!';
   bottomParagraph.textContent = 'Order online or visit us in store!';
   container.appendChild(topParagraph);
