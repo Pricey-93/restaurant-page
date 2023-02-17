a946eeff10f6a13150ca87ab2055e2110a081220
Dan <danieljamieprice@gmail.com>
2023-02-17 22:56:14 +0900

    import lego pizza man image

--- src/home.js
+++ src/home.js
@@ -1,3 +1,5 @@
+import legoPizzaManImage from './lego-pizza-man.png';
+
 export default function createHomeContent() {
   const container = document.createElement('div');
   const topParagraph = document.createElement('p');
@@ -6,6 +8,7 @@ export default function createHomeContent() {
   container.id = 'home-content-container';
   container.classList.add('active-content');
   topParagraph.textContent = 'we\'ve been making the sickest pizzas since 1905!';
+  image.src = legoPizzaManImage;
   bottomParagraph.textContent = 'Order online or visit us in store!';
   container.appendChild(topParagraph);
   container.appendChild(image);
