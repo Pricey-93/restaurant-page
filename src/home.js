aac6a3933f705a39981138f502d0e8d6c0659094
Dan <danieljamieprice@gmail.com>
2023-02-21 23:00:09 +0900

    Add ID to image

--- src/home.js
+++ src/home.js
@@ -7,7 +7,8 @@ export default function createHomeContent() {
   const bottomParagraph = document.createElement('p');
   container.id = 'home-content-container';
   container.classList.add('active-content');
-  topParagraph.textContent = 'we\'ve been making the sickest pizzas since 1905!';
+  topParagraph.textContent = 'we\'ve been making the best lorum ipsum pizzas with love since 1950!';
+  image.id = 'pizza-man';
   image.src = legoPizzaManImage;
   bottomParagraph.textContent = 'Order online or visit us in store!';
   container.appendChild(topParagraph);
