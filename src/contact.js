9f8bedde39b51f974f20ee4b818ed745298a1e4b
Dan <danieljamieprice@gmail.com>
2023-02-21 22:59:03 +0900

    include location image

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,5 @@
+import pizzaPalaceLocation from './pizza-palace-location.png';
+
 export default function createContactContent() {
   const container = document.createElement('div');
   const contactNumber = document.createElement('p');
@@ -5,10 +7,13 @@ export default function createContactContent() {
   const locationImage = document.createElement('img');
   container.id = 'contact-content-container';
   container.classList.add('active-content');
-  contactNumber.textContent = '123456789';
+  locationImage.src = pizzaPalaceLocation;
+  locationImage.width = 300;
+  locationImage.height = 300;
+  contactNumber.textContent = 'Call us: 123456789';
   address.textContent = '317 Abbey Ln, Leicester LE4 5QJ, United Kingdom';
-  container.appendChild(contactNumber);
   container.appendChild(address);
   container.appendChild(locationImage);
+  container.appendChild(contactNumber);
   return container;
 }
